42fe98d3a22ed43cfc1d6cbf73a05903003c06bc
Adam J <63438283+sirpeebs@users.noreply.github.com>
2023-05-31 00:14:25 -0500

    Update playground.tsx

--- src/pages/playground.tsx
+++ src/pages/playground.tsx
@@ -11,8 +11,8 @@ export default function Playground() {
   return (
     <React.Fragment>
       <Head>
-        <title>OpenAI Playground</title>
-        <meta name="description" content="A clone of OpenAI playground." />
+        <title>GPTDEV Playground</title>
+        <meta name="description" content="Play nice...or don't." />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
